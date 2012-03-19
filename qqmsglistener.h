db55992e51184d4d9b589fff5974445f3fc2b774
zhanlang <zhanlangsir@163.com>
2012-03-19 08:48:43 +0800

    修复一个内存泄露问题

--- qqmsglistener.h
+++ qqmsglistener.h
@@ -7,6 +7,9 @@ class QQMsg;
 class QQMsgListener
 {
 public:
+    virtual ~QQMsgListener() {}
+
+public:
     virtual void showOldMsg(QVector<QQMsg*> msg) = 0;
     virtual void showMsg(const QQMsg *msg) = 0;
     virtual QString talkTo() const = 0;
