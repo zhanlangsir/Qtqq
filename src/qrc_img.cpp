759eae140c5f959420565582e1eccdc46099b879
zhanlangsir <zhanlangsir@gmail.com>
2012-08-01 10:05:53 +0800

    调整qq表情排序，和webqq一致

---
+++ src/qrc_img.cpp
@@ -0,0 +1,30 @@
+/****************************************************************************
+** Resource object code
+**
+** Created: Tue Jul 31 21:47:46 2012
+**      by: The Resource Compiler for Qt version 4.8.1
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtCore/qglobal.h>
+
+QT_BEGIN_NAMESPACE
+
+QT_END_NAMESPACE
+
+
+int QT_MANGLE_NAMESPACE(qInitResources_img)()
+{
+    return 1;
+}
+
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_img))
+
+int QT_MANGLE_NAMESPACE(qCleanupResources_img)()
+{
+    return 1;
+}
+
+Q_DESTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qCleanupResources_img))
+
